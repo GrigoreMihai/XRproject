1fab411097ab036b4e5754bda68e4c644f3f4009
cristina.jidovin.itd@gmail.com <cristina.jidovin.itd@gmail.com>
2020-04-16 13:58:13 +0300

    First button showing on screen

---
+++ ARToon/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    /*
+    GameObject controllerGO;
+    HelloARController scriptHelloAR;
+    GameObject mouseGO;
+
+    public void ChangeObjectTypeToMouse()
+    {
+        controllerGO = GameObject.Find("Controller");
+        scriptHelloAR = controllerGO.GetComponent<HelloARComponent>();
+        mouseGO = GameObject.F
+    }
+    */
+}
---
+++ ARToon/Assets/Scripts/UIScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public GameObject canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject myUI = new GameObject();
+        myUI.transform.SetParent(canvas.transform);
+
+        Text myText = myUI.AddComponent<Text>();
+        myText.text = "Go!";
+
+        /*
+        DefaultControls.Resources uiResources = new DefaultControls.Resources();
+        //Set the Panel Background Image someBgSprite;
+        uiResources.background = someBgSprite;
+        GameObject uiPanel = DefaultControls.CreatePanel(uiResources);
+        uiPanel.transform.SetParent(canvas.transform, false);
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
