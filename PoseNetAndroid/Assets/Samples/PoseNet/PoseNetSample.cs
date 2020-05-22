075069573503a879b28aaabc1817f040db9a3ac9
cristina.jidovin.itd@gmail.com <cristina.jidovin.itd@gmail.com>
2020-05-22 22:12:30 +0300

    Added Unity-chan character in scene

--- PoseNetAndroid/Assets/Samples/PoseNet/PoseNetSample.cs
+++ PoseNetAndroid/Assets/Samples/PoseNet/PoseNetSample.cs
@@ -13,11 +13,14 @@ public class PoseNetSample : MonoBehaviour
     [SerializeField] GLDrawer glDrawer = null;
     [SerializeField, Range(0f, 1f)] float threshold = 0.5f;
 
+
     WebCamTexture webcamTexture;
     PoseNet poseNet;
     Vector3[] corners = new Vector3[4];
 
     public PoseNet.Result[] results;
+
+    public AnimateCharacter animateCharacter;
 
     Quaternion imageRotation = new Quaternion();
 
@@ -41,6 +44,8 @@ public class PoseNetSample : MonoBehaviour
 
         Vector3 test = new Vector3(1, 0, 0);
         Debug.Log(imageRotation * test);
+
+        results = new PoseNet.Result[(int)JointIndex.COUNT];
     }
 
     void OnDestroy()
@@ -50,10 +55,68 @@ public class PoseNetSample : MonoBehaviour
         glDrawer.OnDraw -= OnGLDraw;
     }
 
+    /* Copies the data from networkResults to results and calculates the extra positions. */
+    void CalculateJointPoints(PoseNet.Result [] networkResults)
+    {
+        for(int i = 0; i < networkResults.Length; i++)
+        {
+            results[i].position = networkResults[i].position;
+            results[i].confidence = networkResults[i].confidence;
+        }
+
+        /* now calculate the 4 extra points */
+        // neck location
+        results[(int)JointIndex.NECK].position =
+            (results[(int)JointIndex.LEFT_SHOULDER].position +
+             results[(int)JointIndex.RIGHT_SHOULDER].position) / 2f;
+        results[(int)JointIndex.NECK].confidence =
+            Mathf.Min(results[(int)JointIndex.LEFT_SHOULDER].confidence,
+                      results[(int)JointIndex.RIGHT_SHOULDER].confidence);
+        // head location
+        var betweenEars = (results[(int)JointIndex.LEFT_EAR].position +
+                           results[(int)JointIndex.RIGHT_EAR].position) / 2f;
+        var headVect = (betweenEars - results[(int)JointIndex.NECK].position);
+        headVect = Vector3.Normalize(headVect);
+        var noseVect = (results[(int)JointIndex.NOSE].position
+                        - results[(int)JointIndex.NECK].position);
+        results[(int)JointIndex.HEAD].position = results[(int)JointIndex.NECK].position
+            + headVect * (Vector3.Dot(headVect, noseVect));
+        results[(int)JointIndex.HEAD].confidence = Mathf.Min(
+                  Mathf.Min(results[(int)JointIndex.LEFT_EAR].confidence,
+                            results[(int)JointIndex.RIGHT_EAR].confidence),
+                  Mathf.Min(results[(int)JointIndex.NOSE].confidence,
+                            results[(int)JointIndex.NECK].confidence)
+                  );
+
+        var lc = (results[(int)JointIndex.LEFT_HIP].position +
+                  results[(int)JointIndex.RIGHT_HIP].position) / 2f;
+        var lcConf = Mathf.Min(results[(int)JointIndex.LEFT_HIP].confidence,
+                             results[(int)JointIndex.RIGHT_HIP].confidence);
+        // spine location
+        results[(int)JointIndex.SPINE].position
+            = (results[(int)JointIndex.NECK].position + lc) / 2f;
+        results[(int)JointIndex.SPINE].confidence =
+            Mathf.Min(results[(int)JointIndex.NECK].confidence, lcConf);
+        // hip location
+        results[(int)JointIndex.HIP].position
+            = (results[(int)JointIndex.SPINE].position + lc) / 2f;
+        results[(int)JointIndex.HIP].confidence =
+            Mathf.Min(results[(int)JointIndex.SPINE].confidence, lcConf);
+    }
+
     void Update()
     {
         poseNet.Invoke(webcamTexture);
-        results = poseNet.GetResults();
+        var networkResults = poseNet.GetResults();
+        //results = poseNet.GetResults();
+        //results = networkResults;
+
+        cameraView.material = poseNet.transformMat;
+        /* arrange the camera view */
+        cameraView.rectTransform.localEulerAngles = new Vector3(0,0,180);
+
+
+        CalculateJointPoints(networkResults);
 
         Vector3 imageCenter = new Vector3(0.5f, 0.5f, 0);
 
@@ -63,9 +126,8 @@ public class PoseNetSample : MonoBehaviour
             results[i].position = imageRotation * (results[i].position - imageCenter) + imageCenter;
         }
 
-        cameraView.material = poseNet.transformMat;
-        /* arrange the camera view */
-        cameraView.rectTransform.localEulerAngles = new Vector3(0,0,180);
+
+        animateCharacter.SetNetworkResults(results);
 
 
         // set uv
@@ -74,6 +136,36 @@ public class PoseNetSample : MonoBehaviour
         //     1,
         //     TextureToTensor.AspectMode.Fill);
     }
+
+    public static readonly JointIndex[,] Connections = new JointIndex[,]
+    {
+        // HEAD
+        { JointIndex.LEFT_EAR, JointIndex.LEFT_EYE },
+        { JointIndex.LEFT_EYE, JointIndex.NOSE },
+        { JointIndex.NOSE, JointIndex.RIGHT_EYE },
+        { JointIndex.RIGHT_EYE, JointIndex.RIGHT_EAR },
+        // BODY
+        { JointIndex.LEFT_HIP, JointIndex.LEFT_SHOULDER },
+        { JointIndex.LEFT_ELBOW, JointIndex.LEFT_SHOULDER },
+        { JointIndex.LEFT_ELBOW, JointIndex.LEFT_WRIST },
+        { JointIndex.LEFT_HIP, JointIndex.LEFT_KNEE },
+        { JointIndex.LEFT_KNEE, JointIndex.LEFT_ANKLE },
+        { JointIndex.RIGHT_HIP, JointIndex.RIGHT_SHOULDER },
+        { JointIndex.RIGHT_ELBOW, JointIndex.RIGHT_SHOULDER },
+        { JointIndex.RIGHT_ELBOW, JointIndex.RIGHT_WRIST },
+        { JointIndex.RIGHT_HIP, JointIndex.RIGHT_KNEE },
+        { JointIndex.RIGHT_KNEE, JointIndex.RIGHT_ANKLE },
+        { JointIndex.LEFT_SHOULDER, JointIndex.RIGHT_SHOULDER },
+        { JointIndex.LEFT_HIP, JointIndex.RIGHT_HIP },
+        //
+        { JointIndex.HIP, JointIndex.LEFT_HIP },
+        { JointIndex.HIP, JointIndex.RIGHT_HIP },
+        { JointIndex.HIP, JointIndex.SPINE },
+        { JointIndex.SPINE, JointIndex.LEFT_SHOULDER },
+        { JointIndex.SPINE, JointIndex.RIGHT_SHOULDER },
+        { JointIndex.SPINE, JointIndex.NECK },
+        { JointIndex.NECK, JointIndex.HEAD }
+    };
 
     void OnGLDraw()
     {
@@ -85,12 +177,11 @@ public class PoseNetSample : MonoBehaviour
         GL.Begin(GL.LINES);
 
         GL.Color(Color.green);
-        var connections = PoseNet.Connections;
-        int len = connections.GetLength(0);
+        int len = Connections.GetLength(0);
         for (int i = 0; i < len; i++)
         {
-            var a = results[(int)connections[i, 0]];
-            var b = results[(int)connections[i, 1]];
+            var a = results[(int)Connections[i, 0]];
+            var b = results[(int)Connections[i, 1]];
             if (a.confidence >= threshold && b.confidence >= threshold)
             {
                 // GL.Vertex(Leap3(min, max, new Vector3(a.x, 1f - a.y, 0)));
