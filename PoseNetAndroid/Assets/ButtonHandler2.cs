46d232ebf84b2f9a369f95abd865ca11ff701622
Alexandra Rosca <roscamaria.alexandra@yahoo.com>
2020-06-01 21:03:02 +0300

    Camera fix and character change scene added

---
+++ PoseNetAndroid/Assets/ButtonHandler2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler2 : MonoBehaviour
+{
+  public void changeScene() {
+    AnimateCharacter.abc = 2;
+    UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+  }
+}
