04450a8a4f8d5bcfe91d8ce287e7cbd02cefbadd
Alexandra Rosca <roscamaria.alexandra@yahoo.com>
2020-06-08 01:56:16 +0300

    Interface updates

--- PoseNetAndroid/Assets/ButtonHandler.cs
+++ PoseNetAndroid/Assets/ButtonHandler.cs
@@ -6,8 +6,16 @@ using UnityEngine.SceneManagement;
 public class ButtonHandler : MonoBehaviour
 {
     //public AnimateCharacter a;
+    public GameObject ModelObject1;
+    public GameObject ModelObject2;
+
     public void changeScene() {
       AnimateCharacter.abc = 1;
-      UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+      //UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+      ModelObject1.SetActive (true);
+      ModelObject2.SetActive (false);
+    }
+    public void goToMainMenu() {
+      UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
     }
 }
